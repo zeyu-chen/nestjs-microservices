d185f2ea04db6e42e744e03402d89512689dda14
Zeyu Chen <iszeyu.chen@gmail.com>
2024-12-21 01:05:42 +1100

    feat(common, reservations): add logger for common libs and validation for create-reservation

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,10 +1,12 @@
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationsService } from './reservations.service';
-import { ReservationDocument } from './models/reservation.schema';
-import { ReservationSchema } from './models/reservation.schema';
 
 @Module({
   imports: [
@@ -12,6 +14,7 @@ import { ReservationSchema } from './models/reservation.schema';
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
